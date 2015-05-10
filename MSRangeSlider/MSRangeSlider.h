1db3e8677f4a812cd992f85bf1093da5069b06e1
Maksym Shcheglov <maxscheglov@gmail.com>
2015-05-10 15:32:00 +0100

    Defined the MSRangeSlider layers hierarchy.

--- MSRangeSlider/MSRangeSlider.h
+++ MSRangeSlider/MSRangeSlider.h
@@ -10,15 +10,15 @@
 
 @interface MSRangeSlider : UIControl
 
-//! @abstract The left handle's position. default 0.0. this value will be pinned to min/max
+//! @abstract The left handle's position. The default value is 0.0. this value will be pinned to min/max
 @property (nonatomic, assign) CGFloat fromValue;
-//! @abstract The right handle's position. default 1.0. this value will be pinned to min/max
+//! @abstract The right handle's position. The default value is 1.0. this value will be pinned to min/max
 @property (nonatomic, assign) CGFloat toValue;
 //! @abstract The slider's minimum value. The default value is 0.0.
 @property (nonatomic, assign) CGFloat minimumValue;
 //! @abstract The slider's maximum value. The default value is 1.0.
 @property (nonatomic, assign) CGFloat maximumValue;
-//! @abstract The minimum diapason between sliders.
+//! @abstract The minimum diapason between sliders. The default value is 0.1.
 @property (nonatomic, assign) CGFloat minimumInterval;
 
 @property (nonatomic, retain) UIColor *selectedTrackTintColor UI_APPEARANCE_SELECTOR;
--- MSRangeSlider/MSRangeSlider.m
+++ MSRangeSlider/MSRangeSlider.m
@@ -8,14 +8,118 @@
 
 #import "MSRangeSlider.h"
 
+static CGFloat const kRangeSliderTrackHeight = 2.0f;
+static CGFloat const kRangeSliderHeight = 28.0f;
+
 @interface MSRangeSlider ()
 
 @property (nonatomic, strong) CALayer *fromThumbLayer;
 @property (nonatomic, strong) CALayer *toThumbLayer;
 @property (nonatomic, strong) CALayer *trackLayer;
+@property (nonatomic, strong) CALayer *selectedTrackLayer;
 
 @end
 
 @implementation MSRangeSlider
+
++ (BOOL)requiresConstraintBasedLayout
+{
+    return YES;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+
+    if (self) {
+        [self ms_init];
+    }
+
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+
+    if (self) {
+        [self ms_init];
+    }
+
+    return self;
+}
+
+- (CGSize)intrinsicContentSize
+{
+    return CGSizeMake(UIViewNoIntrinsicMetric, kRangeSliderHeight);
+}
+
+#pragma mark - CALayerDelegate
+
+- (void)layoutSublayersOfLayer:(CALayer *)layer
+{
+    if (layer != self.layer) {
+        return;
+    }
+
+    CGFloat width = CGRectGetWidth(self.bounds);
+    CGFloat height = CGRectGetHeight(self.bounds);
+    self.trackLayer.bounds = CGRectMake(0, 0, width, kRangeSliderTrackHeight);
+    self.trackLayer.position = CGPointMake(width / 2, height / 2);
+
+    self.selectedTrackLayer.bounds = CGRectMake(0, 0, width, kRangeSliderTrackHeight);
+    self.selectedTrackLayer.position = CGPointMake(width / 2, height / 2);
+
+    self.fromThumbLayer.bounds = CGRectMake(0, 0, kRangeSliderHeight, kRangeSliderHeight);
+    self.fromThumbLayer.position = CGPointMake(0, height / 2);
+
+    self.toThumbLayer.bounds = CGRectMake(0, 0, kRangeSliderHeight, kRangeSliderHeight);
+    self.toThumbLayer.position = CGPointMake(width, height / 2);
+}
+
+#pragma mark - Private methods
+
+- (void)ms_init
+{
+    self.minimumValue = 0.0;
+    self.maximumValue = 1.0;
+    self.fromValue = 0.0;
+    self.toValue = 1.0;
+    self.minimumInterval = 0.1;
+
+    self.selectedTrackTintColor = [UIColor colorWithRed:0.0 green:122.0 / 255.0 blue:1.0 alpha:1.0];
+    self.trackTintColor = [UIColor lightGrayColor];
+    self.thumbTintColor = [UIColor whiteColor];
+
+    self.trackLayer = [CALayer layer];
+    self.trackLayer.backgroundColor = self.trackTintColor.CGColor;
+    self.trackLayer.cornerRadius = 1.3;
+    [self.layer addSublayer:self.trackLayer];
+
+    self.selectedTrackLayer = [CALayer layer];
+    self.selectedTrackLayer.backgroundColor = self.selectedTrackTintColor.CGColor;
+    self.selectedTrackLayer.cornerRadius = 1.3;
+    [self.layer addSublayer:self.selectedTrackLayer];
+
+    self.fromThumbLayer = [self ms_createThumbLayer];
+    [self.layer addSublayer:self.fromThumbLayer];
+    self.toThumbLayer = [self ms_createThumbLayer];
+    [self.layer addSublayer:self.toThumbLayer];
+}
+
+- (CALayer *)ms_createThumbLayer
+{
+    CALayer *thumbLayer = [CALayer layer];
+
+    thumbLayer.borderColor = [[UIColor lightGrayColor] colorWithAlphaComponent:.4].CGColor;
+    thumbLayer.borderWidth = .5;
+    thumbLayer.cornerRadius = kRangeSliderHeight / 2;
+    thumbLayer.backgroundColor = self.thumbTintColor.CGColor;
+    thumbLayer.shadowColor = [UIColor blackColor].CGColor;
+    thumbLayer.shadowOffset = CGSizeMake(0.0, 3.0);
+    thumbLayer.shadowRadius = 3;
+    thumbLayer.shadowOpacity = 0.2f;
+    return thumbLayer;
+}
 
 @end
