2bf274b2c13f0c4ec730a708d2a2952a813d07e0
Maksym Shcheglov <maxscheglov@gmail.com>
2015-05-16 22:01:00 +0100

    Updated the range slider to support rendering in the interface builder. Rolled back UIAppearance support.

--- MSRangeSlider/MSRangeSlider.h
+++ MSRangeSlider/MSRangeSlider.h
@@ -9,24 +9,30 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+    IB_DESIGNABLE
+
+/**
+ *  @abstract The MSRangeSlider class implements the range slider control.
+ */
 @interface MSRangeSlider : UIControl
 
-//! @abstract The left handle's position. The default value is 0.0. this value will be pinned to min/max.
-@property (nonatomic, assign) CGFloat fromValue;
-//! @abstract The right handle's position. The default value is 1.0. this value will be pinned to min/max.
-@property (nonatomic, assign) CGFloat toValue;
 //! @abstract The slider's minimum value. The default value is 0.0. Should be lower than maximumValue.
-@property (nonatomic, assign) CGFloat minimumValue;
+@property (nonatomic, assign) IBInspectable CGFloat minimumValue;
 //! @abstract The slider's maximum value. The default value is 1.0. Should be higher than minimumValue.
-@property (nonatomic, assign) CGFloat maximumValue;
+@property (nonatomic, assign) IBInspectable CGFloat maximumValue;
 //! @abstract The minimum diapason between sliders. The default value is 0.1.
-@property (nonatomic, assign) CGFloat minimumInterval;
+@property (nonatomic, assign) IBInspectable CGFloat minimumInterval;
 //! @abstract The color used to tint the selected range.
-@property (nonatomic, strong) UIColor *selectedTrackTintColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong) IBInspectable UIColor *selectedTrackTintColor;// UI_APPEARANCE_SELECTOR;
 //! @abstract The color used to tint the unselected range.
-@property (nonatomic, strong) UIColor *trackTintColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong) IBInspectable UIColor *trackTintColor;// UI_APPEARANCE_SELECTOR;
 //! @abstract The color used to tint the thumbs.
-@property (nonatomic, strong) UIColor *thumbTintColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong) IBInspectable UIColor *thumbTintColor;// UI_APPEARANCE_SELECTOR;
+//! @abstract The left handle's position. The default value is 0.0. this value will be pinned to min/max.
+@property (nonatomic, assign) IBInspectable CGFloat fromValue;
+//! @abstract The right handle's position. The default value is 1.0. this value will be pinned to min/max.
+@property (nonatomic, assign) IBInspectable CGFloat toValue;
 
 @end
+
 NS_ASSUME_NONNULL_END
