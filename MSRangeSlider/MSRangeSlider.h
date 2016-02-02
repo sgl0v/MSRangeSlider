539f4210aec0b0d182fa223533b808804c90ec13
Maksym Shcheglov <maxscheglov@gmail.com>
2016-02-02 21:48:00 +0100

    Updated the slider's documentation.

--- MSRangeSlider/MSRangeSlider.h
+++ MSRangeSlider/MSRangeSlider.h
@@ -12,25 +12,41 @@ NS_ASSUME_NONNULL_BEGIN
     IB_DESIGNABLE
 
 /**
- *  @abstract The MSRangeSlider class implements the range slider control.
+ *  The MSRangeSlider class implements the range slider control.
  */
 @interface MSRangeSlider : UIControl
 
-//! @abstract The slider's minimum value. The default value is 0.0. Should be lower than maximumValue.
+/**
+*  The slider's minimum value. The default value is 0.0. Should be lower than maximumValue.
+*/
 @property (nonatomic, assign) IBInspectable CGFloat minimumValue;
-//! @abstract The slider's maximum value. The default value is 1.0. Should be higher than minimumValue.
+/**
+ *  The slider's maximum value. The default value is 1.0. Should be higher than minimumValue.
+ */
 @property (nonatomic, assign) IBInspectable CGFloat maximumValue;
-//! @abstract The minimum diapason between sliders. The default value is 0.1.
+/**
+ *  The minimum diapason between sliders. The default value is 0.1.
+ */
 @property (nonatomic, assign) IBInspectable CGFloat minimumInterval;
-//! @abstract The color used to tint the selected range.
+/**
+ *  The color used to tint the selected range.
+ */
 @property (nonatomic, strong) IBInspectable UIColor *selectedTrackTintColor;// UI_APPEARANCE_SELECTOR;
-//! @abstract The color used to tint the unselected range.
+/**
+ *  The color used to tint the unselected range.
+ */
 @property (nonatomic, strong) IBInspectable UIColor *trackTintColor;// UI_APPEARANCE_SELECTOR;
-//! @abstract The color used to tint the thumbs.
+/**
+ *  The color used to tint the thumbs.
+ */
 @property (nonatomic, strong) IBInspectable UIColor *thumbTintColor;// UI_APPEARANCE_SELECTOR;
-//! @abstract The left handle's position. The default value is 0.0. this value will be pinned to min/max.
+/**
+ *  The left handle's position. The default value is 0.0. this value will be pinned to min/max.
+ */
 @property (nonatomic, assign) IBInspectable CGFloat fromValue;
-//! @abstract The right handle's position. The default value is 1.0. this value will be pinned to min/max.
+/**
+ *  The right handle's position. The default value is 1.0. this value will be pinned to min/max.
+ */
 @property (nonatomic, assign) IBInspectable CGFloat toValue;
 
 @end
